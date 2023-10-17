e61a3d35a2bedcdbb48d0c6355273db59edb5a6d
Rafael Magalhaes <0000980036@senaimgaluno.com>
2023-10-16 22:19:22 -0300

    comit boletinho

--- src/pages/atividadesemsala/script-boletim.js
+++ src/pages/atividadesemsala/script-boletim.js
@@ -3,30 +3,37 @@ let materia = { id: 0, nome: "", notas: [], total: 0, printInfo: "" };
 let materias = [];
 let user = {nome: "",instituicao:"", curso:""};
 function save() {
-    let inputNotas = [];
+    
+    let inputNotas = document.querySelectorAll("div.input-group > input");
+    let notas = [];
     let totalNotas = 0;
     user.nome = nomeUsuario.value;
     user.instituicao = nomeInstituicao.value;
     let resultados = `<b>BOLETIM DE: ${user.nome.toLocaleUpperCase()}</b><br><b>INSTITUIÇÃO/ESCOLA: ${user.instituicao.toLocaleUpperCase()} </b>`;
 
-    document.querySelectorAll("div.input-group input").forEach(e => {
-        inputNotas.push(parseFloat(e.value))
+    console.log(inputNotas)
+
+    inputNotas.forEach(e => {
+        notas.push(parseFloat(e.value));
         totalNotas += parseFloat(e.value);
     })
+
+    console.log(notas)
+
 
     materias.push(
         {
             id: materia.id++,
             nome: nomeMateria.value.toLocaleUpperCase(),
-            notas: inputNotas,
+            notas: notas,
             total: totalNotas,
             info: `<hr>
                     RESULTADO DE: <b>${nomeMateria.value.toLocaleUpperCase()}</b> 
                     <ul class="list-group">
-                        <li class="list-group-item"><b>1º Bimestre:</b> ${inputNotas[0]}</li>
-                        <li class="list-group-item"><b>2º Bimestre:</b> ${inputNotas[1]}</li>
-                        <li class="list-group-item"><b>3º Bimestre:</b> ${inputNotas[2]}</li>
-                        <li class="list-group-item"><b>4º Bimestre:</b> ${inputNotas[3]}</li>
+                        <li class="list-group-item"><b>1º Bimestre:</b> ${notas[0]}</li>
+                        <li class="list-group-item"><b>2º Bimestre:</b> ${notas[1]}</li>
+                        <li class="list-group-item"><b>3º Bimestre:</b> ${notas[2]}</li>
+                        <li class="list-group-item"><b>4º Bimestre:</b> ${notas[3]}</li>
                         <li class="list-group-item">TOTAL FINAL: <b>${totalNotas}</b></li>
                         <li class="list-group-item">STATUS: <b>${isAprovadoOuReprovado(totalNotas)}</b></li>
                     </ul>
@@ -50,4 +57,3 @@ function isAprovadoOuReprovado(valor) {
     }
 }
 
-<button onclick="save()">Verificar Notas</button>
